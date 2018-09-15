fde6a040050bce242e4ef9e664a0b52ec6ca96e7
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-09-15 17:15:13 -0400

    A̧̡͉̺͈̠͈͘A̶̠̫̜̬̫͈A̻̰͚̪͕̫̱ͅA̪̥̮̜͢͝A͇̦͕̗͠A̭̹̖͇̭͙͔̥͜͜͡À̸͚̗̠̲͠A̸̬̯̦͔Á̷̮͙Ą̶̖̯̘̭̀A҉͕̩̲̫̪̬̰̀Á̰̥̤̭̕͘À̷̗̩̠͙̫̻̙̥̭̕A̢̝̪̳̤͚̖̖H̴̦̭͞ͅH̛̤̘̫̞͈̲Ḩ̳̜̗̹̠H̛̘͎͇͖̲̪́H͏̠̪͉͔̥̯͖H̸̴͍̲̣̜̗͞Ḩ̮̗͖̩̠͉̩͝!̫͎̻̳̱̣̕͘ ҉̳  (Sorry forgot about this)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("Please enter an integer. Ex: 12, 7, 3");
             kbinput = new Scanner(System.in);
         }
-        
+
         int userIn = kbinput.nextInt();
         int oddOrEven = userIn % 2;
 
