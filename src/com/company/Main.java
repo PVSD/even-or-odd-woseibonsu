724dfffba7d8d829e2ca909e5df5d27e8f645b25
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-09-15 17:08:59 -0400

    A̧̡͉̺͈̠͈͘A̶̠̫̜̬̫͈A̻̰͚̪͕̫̱ͅA̪̥̮̜͢͝A͇̦͕̗͠A̭̹̖͇̭͙͔̥͜͜͡À̸͚̗̠̲͠A̸̬̯̦͔Á̷̮͙Ą̶̖̯̘̭̀A҉͕̩̲̫̪̬̰̀Á̰̥̤̭̕͘À̷̗̩̠͙̫̻̙̥̭̕A̢̝̪̳̤͚̖̖H̴̦̭͞ͅH̛̤̘̫̞͈̲Ḩ̳̜̗̹̠H̛̘͎͇͖̲̪́H͏̠̪͉͔̥̯͖H̸̴͍̲̣̜̗͞Ḩ̮̗͖̩̠͉̩͝!̫͎̻̳̱̣̕͘ ҉̳  (Sorry forgot to commit this)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Enter an integer: ");
+        Scanner kbinput = new Scanner(System.in);
+
+        while(!kbinput.hasNextInt())
+        {
+            System.out.println("Please enter an integer. Ex: 12, 7, 3");
+            kbinput = new Scanner(System.in);
+        }
+
+        int userIn = kbinput.nextInt();
+        int oddOrEven = userIn % 2;
+
+        if(oddOrEven == 0)
+        {
+            System.out.println("The integer " + userIn + " is even." );
+        }
+        else
+        {
+            System.out.println("The integer " + userIn + " is odd.");
+        }
+
+
+
+
+
     }
 }
